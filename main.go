921d3feb409e872350385bb4bf568f7f16973677
asus <auliakhoirunnisa03@gmail.com>
2024-04-22 19:27:34 +0700

    first init github

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	tax = 10
+	app = 2000
+)
+
+func main() {
+	fmt.Println(HitungHargaTotal(15000, 10000, 2))
+	err := PembayaranBarang(600000, "credit", true)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Pembayaran berhasil!")
+	}
+}
+
+func HitungHargaTotal(hargaItem, ongkir float64, qty int) (float64, error) {
+	if hargaItem <= 0 {
+		return 0, errors.New("harga barang tidak boleh nol")
+	}
+
+	if qty <= 0 {
+		return 0, errors.New("jumlah barang tidak boleh nol")
+	}
+
+	// 15000 * 2 = 30000
+	hargaAkhirItem := hargaItem * float64(qty)
+
+	if ongkir <= 0 {
+		return 0, errors.New("harga ongkir tidak boleh nol")
+	}
+
+	// 30000 + 10000
+	hargaSetelahOngkir := hargaAkhirItem + ongkir
+	// 40000 + 3000
+	pajak := hargaAkhirItem * tax / 100
+	total := hargaSetelahOngkir + pajak + app
+
+	return total, nil
+}
+
+func PembayaranBarang(hargaTotal float64, metodePembayaran string, dicicil bool) error {
+	if hargaTotal <= 0 {
+		return errors.New("harga tidak bisa nol")
+	}
+
+	validMetodes := map[string]bool{"cod": true, "transfer": true, "debit": true, "credit": true, "gerai": true}
+	if !validMetodes[metodePembayaran] {
+		return errors.New("metode tidak dikenali")
+	}
+
+	if metodePembayaran == "credit" && !dicicil {
+		return errors.New("credit harus dicicil")
+	}
+
+	if dicicil && metodePembayaran != "credit" {
+		return errors.New("metode pembayaran harus credit untuk dicicil")
+	}
+
+	if dicicil && hargaTotal < 500000 {
+		return errors.New("harga total harus >= 500.000 untuk pembayaran dicicil")
+	}
+
+	return nil
+}
---
+++ main_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHitungHargaTotal(t *testing.T) {
+	type args struct {
+		hargaItem float64
+		ongkir    float64
+		qty       int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "failed in harga item",
+			args: args{
+				hargaItem: 0,
+				ongkir:    1,
+				qty:       1,
+			},
+			want:    0,
+			wantErr: true,
+		},
+
+		{
+			name: "failed in qty",
+			args: args{
+				hargaItem: 1,
+				ongkir:    1,
+				qty:       0,
+			},
+			want:    0,
+			wantErr: true,
+		},
+
+		{
+			name: "failed in ongkir",
+			args: args{
+				hargaItem: 1,
+				ongkir:    0,
+				qty:       1,
+			},
+			want:    0,
+			wantErr: true,
+		},
+
+		{
+			name: "sukses",
+			args: args{
+				hargaItem: 15000,
+				ongkir:    10000,
+				qty:       2,
+			},
+			want:    45000,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := HitungHargaTotal(tt.args.hargaItem, tt.args.ongkir, tt.args.qty)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HitungHargaTotal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("HitungHargaTotal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPembayaranBarang(t *testing.T) {
+	tests := []struct {
+		name             string
+		hargaTotal       float64
+		metodePembayaran string
+		dicicil          bool
+		wantErr          string
+	}{
+		{
+			name:             "failed harga tidak boleh nol",
+			hargaTotal:       0,
+			metodePembayaran: "cod",
+			dicicil:          false,
+			wantErr:          "harga tidak bisa nol",
+		},
+		{
+			name:             "failed metode pembayaran tidak dikenali",
+			hargaTotal:       10000,
+			metodePembayaran: "emoney",
+			dicicil:          false,
+			wantErr:          "metode tidak dikenali",
+		},
+		{
+			name:             "failed metode pembayaran credit harus dicicil",
+			hargaTotal:       200000,
+			metodePembayaran: "credit",
+			dicicil:          false,
+			wantErr:          "credit harus dicicil",
+		},
+		{
+			name:             "success",
+			hargaTotal:       600000,
+			metodePembayaran: "credit",
+			dicicil:          true,
+			wantErr:          "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := PembayaranBarang(tt.hargaTotal, tt.metodePembayaran, tt.dicicil)
+			if (err != nil && err.Error() != tt.wantErr) || (err == nil && tt.wantErr != "") {
+				t.Errorf("PembayaranBarang() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
